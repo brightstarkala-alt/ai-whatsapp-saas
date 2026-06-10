2a913d9065a32dd70f0bca2d0fccd684eeda9ce0
brightstarkala <brightstar.kala@gmail.com>
2026-06-10 17:21:59 +0530

    Fix clients module registration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KnowledgeController } from './knowledge/knowledge.controller';
 import { WhatsappController } from './whatsapp/whatsapp.controller';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule],
   controllers: [
     AppController,
     KnowledgeController,
